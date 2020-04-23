1a4918614b630a5099266a38ed9ee1e68982026d
P.S.S.ABISHEK <pssabishek18@gmail.com>
2020-04-23 20:08:22 +0530

    Updates

--- controller/inboxcontroller.js
+++ controller/inboxcontroller.js
@@ -1,23 +1,28 @@
-﻿var express = require('express');
-var mongoose = require('mongoose');
-var schema = require('../models/schema.js');
+﻿var express = require("express");
+var mongoose = require("mongoose");
+var schema = require("../models/schema.js");
 var Inbox = mongoose.model("schema");
 var inboxcontroller = {};
 
-
 /* save new employee function */
 
-inboxcontroller.save = function (req, res) {
+inboxcontroller.save = function(req, res) {
+  console.log(req);
+  
+  if(req.body.name || req.body.email || req.body.message) {
     var inbox = new Inbox(req.body);
-
-    inbox.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Successfully entered");
-            res.redirect("/");
-        }
+    inbox.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully entered");
+        res.redirect("/");
+      }
     });
+  }
+  else {
+    res.redirect('/')
+  }
 };
 
 module.exports = inboxcontroller;
